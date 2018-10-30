22e7072354dcc014a3ce43ef0e7025ad80881015
Rachel D. Cartwright <Rachel.D.Cartwright@gmail.com>
2018-10-29 23:57:29 -0500

    set up to to work with api - working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Email Logic' });
+  return fetch('https://api.salesloft.com/v2/people.json', {
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + process.env.API_KEY,
+      'Content-Type': 'application/json'
+    },
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      console.log('result', result);
+      return res.json(result);
+    })
+    .catch((err) => {
+      console.log('err', err);
+      return res.json(err);
+    });
 });
 
 module.exports = router;
