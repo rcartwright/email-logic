b48024391daef79656d619e663e9a3675591c2c1
Rachel D. Cartwright <Rachel.D.Cartwright@gmail.com>
2018-10-30 22:15:53 -0500

    render html page instead of json & change people page count

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,18 @@ var fetch = require('node-fetch');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  return fetch('https://api.salesloft.com/v2/people.json', {
+  return fetch('https://api.salesloft.com/v2/people.json?per_page=100', {
     method: 'GET',
     headers: {
       'Authorization': 'Bearer ' + process.env.API_KEY,
       'Content-Type': 'application/json'
-    },
+    }
   })
     .then((res) => res.json())
     .then((result) => {
       console.log('result', result);
-      return res.json(result);
+      //return res.json(result.data);
+      return res.render('index', { people: result.data });
     })
     .catch((err) => {
       console.log('err', err);
