f119a18730eeebe5e7385e06ff5bea82a171a05f
Rachel D. Cartwright <Rachel.D.Cartwright@gmail.com>
2018-11-04 19:51:07 -0600

    show character count, more styling

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var fetch = require('node-fetch');
+var { getEmailCharCount } = require('../helpers');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  return fetch('https://api.salesloft.com/v2/people.json?per_page=100', {
+  return fetch('https://api.salesloft.com/v2/people.json?per_page=25', {
     method: 'GET',
     headers: {
       'Authorization': 'Bearer ' + process.env.API_KEY,
@@ -14,7 +15,10 @@ router.get('/', function (req, res, next) {
     .then((res) => res.json())
     .then((result) => {
       //return res.json(result.data);
-      return res.render('index', { people: result.data });
+      return res.render('index', {
+        people: result.data,
+        characterDetails: getEmailCharCount(result.data)
+      });
     })
     .catch((err) => {
       console.log('err', err);
